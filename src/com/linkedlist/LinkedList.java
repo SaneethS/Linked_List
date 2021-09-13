79fa0631d4a82d5ee36eb3ae22583164b7b137a7
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-09-13 14:23:51 +0530

    UC2 has been created where the values are added to the linked list through push

--- src/com/linkedlist/LinkedList.java
+++ src/com/linkedlist/LinkedList.java
@@ -13,4 +13,22 @@ public class LinkedList {
 			next = null;
 		}
 	}
+	
+	public void push(int data) {
+		Node newNode = new Node(data);
+		newNode.next = head;
+		head = newNode;
+		size++;
+	}
+	
+	public void print() {
+		Node temp = head;
+		
+		System.out.println("LinkedList : ");
+		
+		while(temp != null) {
+			System.out.print(temp.data+"->");
+			temp = temp.next;
+		}
+	}
 }
