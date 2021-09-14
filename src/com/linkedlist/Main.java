6d98443db62b6bc583615073d6cabccca1b8f359
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-09-14 18:11:39 +0530

    UC10 has been created where sorted linked list is created and generic methods used

--- src/com/linkedlist/Main.java
+++ src/com/linkedlist/Main.java
@@ -2,22 +2,30 @@ package com.linkedlist;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Welcome to linked list");
-		LinkedList list = new LinkedList();
+//		System.out.println("Welcome to linked list");
+//		LinkedList list = new LinkedList();
+//		
+//		list.push(70);
+//		list.push(30);
+//		list.push(56);
+//		
+//		list.insertAtPos(3, 40);
+//		
+//		list.remove(40);
+//		
+//		list.print();
+//		
+//		System.out.println("\n"+list.search(30));
+//		
+//		System.out.println(list.size());
 		
-		list.push(70);
-		list.push(30);
-		list.push(56);
+		SortedLinkedList<Integer> sortedList = new SortedLinkedList<>();
 		
-		list.insertAtPos(3, 40);
+		sortedList.add(70);
+		sortedList.add(30);
+		sortedList.add(56);
 		
-		list.remove(40);
-		
-		list.print();
-		
-		System.out.println("\n"+list.search(30));
-		
-		System.out.println(list.size());
+		sortedList.print();
 		
 		
 	}
